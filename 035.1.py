c3a66125f98cecedd98da02e588207d56fc197d0
kamibarreto <fabinhobarreto9928@gmail.com>
2020-07-14 14:19:41 -0300

    exercicios

---
+++ 035.1.py
@@ -0,0 +1,10 @@
+print('-=' * 20)
+print('Analisando triangulos...')
+print('-=' * 20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos podem formar um traingulo')
+else:
+    print('Os segmentos não podem formar um triangulo')
--- 035.py
+++ 035.py
@@ -1,6 +1,6 @@
-lin1 = int(input('Digite o comprimento da primeira reta: '))
-lin2 = int(input('Digite o comprimento da segunda reta: '))
-lin3 = int(input('Digite o comprimento da terceira reta: '))
+lin1 = float(input('Digite o comprimento da primeira reta: '))
+lin2 = float(input('Digite o comprimento da segunda reta: '))
+lin3 = float(input('Digite o comprimento da terceira reta: '))
 if lin1 < lin2 + lin3:
     if lin2 < lin1 + lin3:
         if lin3 < lin1 + lin2:
