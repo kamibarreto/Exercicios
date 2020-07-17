13e17b6de7d8f74cb9b1bb79b15f75f8122116e8
kamibarreto <fabinhobarreto9928@gmail.com>
2020-07-17 15:31:50 -0300

    exercicio

--- 017.py
+++ 017.py
@@ -1,7 +1,15 @@
-from math import hypot
+'''from math import hypot
 cat1 = float(input('Digite o cateto oposto: '))
 cat2 = float(input('Digite o cateto adjacente: '))
 n = hypot(cat1, cat2)
 print("a hipotenusa é igual á: {:.2f}".format(n))
 n2 = (cat1 ** 2 + cat2 **2) ** (1/2)
-print("a hiptotenusa é igual á: {:.2f}".format(n2))
+print("a hiptotenusa é igual á: {:.2f}".format(n2))'''
+from math import hypot
+a = float (input('Digite o valor do cateto oposto '))
+b = float (input('Digite o valor do cateto adjacente '))
+d = a * 2
+e = b * 2
+f = d + e
+c = f ** (1/2)
+print('{}'.format(c))
